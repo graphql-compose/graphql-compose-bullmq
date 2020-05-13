c1c54fdedda79c89f94f995ef2b1f93e21acd406
nodkz <pavel.chertorogov@gmail.com>
2020-05-13 19:55:36 +0600

    fix: check Redis connection also by constructor name (helps to avoid multiple ioredis packages installation)

--- src/helpers/getBullConnection.ts
+++ src/helpers/getBullConnection.ts
@@ -4,20 +4,21 @@ import Redis from 'ioredis';
 const connectionMap = new Map<Options['redis'], Redis.Redis>();
 
 export function getBullConnection(opts: Options): Redis.Redis {
-  let connection = connectionMap.get(opts.redis);
+  const optsRedis = opts?.redis;
+  let connection = connectionMap.get(optsRedis);
   if (connection) {
     return connection;
   }
 
-  if (opts?.redis instanceof Redis) {
-    connection = opts.redis;
-  } else if (opts?.redis) {
-    connection = new Redis(opts.redis);
+  if (optsRedis instanceof Redis || optsRedis?.constructor?.name === 'Redis') {
+    connection = optsRedis as Redis.Redis;
+  } else if (optsRedis) {
+    connection = new Redis(optsRedis);
   } else {
     connection = new Redis();
   }
 
-  connectionMap.set(opts.redis, connection);
+  connectionMap.set(optsRedis, connection);
 
   return connection;
 }
