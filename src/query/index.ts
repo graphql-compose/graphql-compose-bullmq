c7a760d28ab8f3e742309e93ee4217798861c6d8
intpro <boris.dorofeev@gmail.com>
2020-06-01 03:28:45 +0600

    feat: add redis memory info query (#16)
    Co-authored-by: Boris Dorofeev <bdorofeev@bdorofeev-laptop.corp.ps.kz>

--- src/query/index.ts
+++ src/query/index.ts
@@ -2,3 +2,4 @@ export { createQueuesFC } from './queues';
 export { createQueueKeysFC } from './queueKeys';
 export { createQueueFC } from './queue';
 export { createJobFC } from './job';
+export { createInfoFC } from './info';
