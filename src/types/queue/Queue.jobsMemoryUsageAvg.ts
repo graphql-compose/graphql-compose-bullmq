a207c0f56671940a1dc5b6f14d201ae8d99e7d11
Boris Dorofeev <bdorofeev@bdorofeev-laptop.corp.ps.kz>
2020-06-02 23:22:59 +0600

    feat: add jobs memory usage avg field

---
+++ src/types/queue/Queue.jobsMemoryUsageAvg.ts
@@ -0,0 +1,41 @@
+import { SchemaComposer, ObjectTypeComposerFieldConfigDefinition } from 'graphql-compose';
+import { Queue } from 'bullmq';
+import { Options } from '../../definitions';
+import { getZsetKeysMemoryUsageAvg } from '../../helpers';
+
+export function createJobsMemoryUsageAvgFC(
+  sc: SchemaComposer<any>,
+  opts: Options
+): ObjectTypeComposerFieldConfigDefinition<any, any> {
+  return {
+    type: 'Int!',
+    args: {
+      keySetName: {
+        type: sc.createEnumTC({
+          name: 'keySetNamesEnum',
+          values: {
+            COMPLETED: { value: 'completed' },
+            FAILED: { value: 'failed' },
+          },
+        }),
+        defaultValue: 'completed',
+      },
+      limit: {
+        type: 'Int',
+        defaultValue: 100,
+      },
+    },
+    resolve: async (queue: Queue, { limit, keySetName }) => {
+      const avgBytes =
+        (await getZsetKeysMemoryUsageAvg(
+          queue.opts?.prefix || 'bull',
+          queue.name,
+          keySetName,
+          limit,
+          opts
+        )) || 0;
+
+      return avgBytes.toFixed(0);
+    },
+  };
+}
